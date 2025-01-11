a30eb86e870c5ce14a996a26e8cc2fd33fd7e36f
gonzaloezemolina <gonzaloezemolina@gmail.com>
2025-01-11 19:06:53 -0300

    implement initial logic in product customization

--- src/static/index.js
+++ src/static/index.js
@@ -6,46 +6,94 @@ menuToggle.addEventListener('click', () => {
     nav.classList.toggle('active');
 });
 
-const customLeft = [
-    {
-        title: 'Personaliza tu cuaderno o agenda',
-        text: 'Elegí entre una de las opciones y ve continuando a lo largo de la pasarela'
+const state = {
+    currentStep: 1,
+    selectedOptions: {
+        productType: null,
+        cover: null, 
+        interior: null,
+        binding: null,
+        accessories: [],
+        customNotes: null,
+        price: 0,
     },
-    {
-        title: 'Anillado o cocido',
-        text: 'Elegí entre una de las opciones y ve continuando a lo largo de la pasarela'
-    },
-    {
-        title: 'Selecciona una tapa',
-        text: 'Puedes agregar la tuya o puedes hacerlo con una imagen preestablecida'
-    },
-];
+};
 
-const getCustomLeft = document.getElementById('custom_left');
+const renderStep = () => {
+    const leftDiv = document.getElementById('custom_left');
+    const rightDiv = document.getElementById('custom_right');
 
-customLeft.forEach((item) => {
-    const div = document.createElement('div');
-    div.innerHTML = `<h3>${item.title}</h3>
-    <p>${item.text}</p>
-    `
-    getCustomLeft.appendChild(div);
-});
+    switch (state.currentStep) {
+        case 1:
+            leftDiv.innerHTML = `
+            <h2>Elige entre cuaderno o agenda. Paso ${state.currentStep}/6</h2>
+            <p>Selecciona el tipo de producto que quieres personalizar</p>
+            `;
+            rightDiv.innerHTML = `
+                <button onclick="selectProduct('cuaderno')">Cuaderno</button>
+                <button onclick="selectProduct('agenda')">Agenda</button>
+            `;
+            break;
+        case 2:
+            leftDiv.innerHTML = `<h2>Elige la tapa.  Paso ${state.currentStep}/6</h2>`;
+            rightDiv.innerHTML = `
+            <button onclick="goToPreviousStep()">Volver</button>
+            <p>TAPA1</p> 
+            <button onclick="goToNextStep()">Siguiente</button>
+            `
+            break;
+        case 3:
+            leftDiv.innerHTML = `<h2>Elige el interior.  Paso ${state.currentStep}/6</h2>`;
+            rightDiv.innerHTML = `
+            <button onclick="goToPreviousStep()">Volver</button>
+            <p>INTERIOR1</p> 
+            <button onclick="goToNextStep()">Siguiente</button>
+            `
+            break;
+        case 4:
+            leftDiv.innerHTML = `<h2>Elige el tipo de encuadernación.  Paso ${state.currentStep}/6</h2>`;
+            rightDiv.innerHTML = `
+            <button onclick="goToPreviousStep()">Volver</button>
+            <p>ENCUADERNACION1</p>
+            <button onclick="goToNextStep()">Siguiente</button> 
+            `
+            break;
+        case 5:
+            leftDiv.innerHTML = `<h2>Elige los accesorios.  Paso ${state.currentStep}/6</h2>`;
+            rightDiv.innerHTML = `
+            <button onclick="goToPreviousStep()">Volver</button>
+            <p>ACCESORIO1</p>
+            <button onclick="goToNextStep()">Siguiente</button> 
+            `
+            break;
+        case 6:
+            leftDiv.innerHTML = `<h2>Personaliza tu producto.  Paso ${state.currentStep}/6</h2>`;
+            rightDiv.innerHTML = `
+                <button onclick="goToPreviousStep()">Volver</button>
+                    <textarea placeholder="Escribe aquí tus notas"></textarea>
+                <button onclick="goToNextStep()">Siguiente</button>
+            `;
+            break
+        default:
+            leftDiv.innerHTML = "<h2>Finalizado</h2>";
+            rightDiv.innerHTML = `<button onclick="addToCart()">Agregar al carrito</button>`;
+    }
+};
 
+const selectProduct = (product) => {
+    state.selectedOptions.productType = product;
+    state.currentStep++;
+    renderStep();
+};
 
+const goToNextStep = () => {
+    state.currentStep++;
+    renderStep();
+};
 
-const steps = [];
+const goToPreviousStep = () => {
+    if (state.currentStep > 1) state.currentStep--;
+    renderStep();
+};
 
-
-
-
-
-
-
-
-//  Vamos a crear con PostgreSQL las distintas opciones. Una vez que conseguimos las opciones, vamos a por todo:
-//  1. Posiblemente tengamos un switch con distintos cases + break. 
-// 2. El programa probablemente de error si le damos a varios endpoints a la vez. El fetch debe aparecer en el momento dado. no viene mal intentar una funcion y activarla despues de cada break
-// 3. De alguna forma u otra debemos implementar un 0/6. Tambien se debe volver para atras
-// 4. El usuario debe poder agregar la imagen de una tapa y poder eliminarla tambien.
-// 5. Se debe crear un objeto con todas las opciones seleccionadas
-// 6. Tema precio. Hay que averiguar como se puede ir aumentando respecto a cada opcion
+renderStep();
