8353a628a679367ce12882a1495cdb79302aef4c
gonzaloezemolina <gonzaloezemolina@gmail.com>
2025-01-12 20:25:58 -0300

    implement basic html in index.js

--- src/static/index.js
+++ src/static/index.js
@@ -45,6 +45,18 @@ const renderStep = () => {
             rightDiv.innerHTML = `
             <div class="covers_custom_container">
             </div>
+            <div class="covers_options">
+            Aqui hay que realizar el foreach de las tapas, primero realizar el fetch de las tapas
+            <div class="covers_buttons">
+            <button>Añadir mi imagen</button>
+            <button>Añadir texto</button>
+            <select>
+                <option value="monserrat">Monserrat</option>
+                <option value="roboto">Roboto</option>
+            </select>
+            <button>Color</button>
+            </div>
+            </div>
             <div>
                 <button onclick="goToPreviousStep()" class="btn_step">Volver</button>
                 <button onclick="goToNextStep()" class="btn_step">Siguiente</button>
@@ -55,9 +67,14 @@ const renderStep = () => {
             if (state.selectedOptions.productType === 'cuaderno') {
                 leftDiv.innerHTML = `<h2>Elige el interior.  Paso ${state.currentStep}/6</h2>`;
                 rightDiv.innerHTML = `<div class="interior_custom_container">
-                    <img src="" alt="imagen de interior de cuaderno" class="interior_custom">
-                    <img>
-                    <img>
+                <figure>
+                    <img src="./media/lisa.jpg" alt="imagen de interior de cuaderno" class="interior_custom" width="100px" height="100px">
+                    <figcaption>Lisa</figcaption>
+                    <img src="./media/rayada.jpg" alt="imagen de interior de cuaderno" class="interior_custom" width="100px" height="100px">
+                    <figcaption>Rayada</figcaption>
+                    <img src="./media/cuadriculada.jpg" alt="imagen de interior de cuaderno" class="interior_custom" width="100px" height="100px">
+                    <figcaption>Cuadriculada</figcaption>
+                </figure>
                 </div>
                 <div>
                     <button onclick="goToPreviousStep()" class="btn_step">Volver</button>
@@ -78,6 +95,14 @@ const renderStep = () => {
         case 4:
             leftDiv.innerHTML = `<h2>Elige el tipo de encuadernación.  Paso ${state.currentStep}/6</h2>`;
             rightDiv.innerHTML = `
+            <div class="binding_custom_container">
+            <figure>
+                <img src="./media/anillado.jpeg" alt="imagen de encuadernación" class="binding_custom" width="130px" height="130px">
+                <figcaption>Anillado</figcaption>
+                <img src="./media/cocido.jpeg" alt="imagen de encuadernación" class="binding_custom" width="130px" height="130px">
+                <figcaption>Cocido</figcaption>
+            </figure>
+            </div>
                         <div>
                 <button onclick="goToPreviousStep()" class="btn_step">Volver</button>
                 <button onclick="goToNextStep()" class="btn_step">Siguiente</button>
@@ -87,17 +112,29 @@ const renderStep = () => {
         case 5:
             leftDiv.innerHTML = `<h2>Elige los accesorios.  Paso ${state.currentStep}/6</h2>`;
             rightDiv.innerHTML = `
-                        <div>
+            <div>
+                <input type="checkbox" id="accesorio1" name="accesorio1" value="accesorio1">
+                <label for="accesorio1">Accesorio 1</label>
+                <input type="checkbox" id="accesorio2" name="accesorio2" value="accesorio2">
+                <label for="accesorio2">Accesorio 2</label>
+                <input type="checkbox" id="accesorio3" name="accesorio3" value="accesorio3">
+                <label for="accesorio3">Accesorio 3</label>
+                <input type="checkbox" id="accesorio4" name="accesorio4" value="accesorio4">
+                <label for="accesorio4">Accesorio 4</label>
+            </div>
+            <div>
                 <button onclick="goToPreviousStep()" class="btn_step">Volver</button>
                 <button onclick="goToNextStep()" class="btn_step">Siguiente</button>
             </div>
             `
             break;
         case 6:
-            leftDiv.innerHTML = `<h2>Personaliza tu producto.  Paso ${state.currentStep}/6</h2>`;
+            leftDiv.innerHTML = `<h2>Haz alguna aclaración.  Paso ${state.currentStep}/6</h2>`;
             rightDiv.innerHTML = `
+            <div class="custom_notes_container">
                     <textarea placeholder="Escribe aquí tus notas"></textarea>
-                            <div>
+            </div>
+                <div>
                 <button onclick="goToPreviousStep()" class="btn_step">Volver</button>
                 <button onclick="goToNextStep()" class="btn_step">Siguiente</button>
             </div>
